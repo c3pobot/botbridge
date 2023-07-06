6cccab9bd209d16007f49d9657b681021c50ed85
scuba <scuba75@gmail.com>
2023-07-06 19:17:28 +0000

    update

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
-const { Bridge } = require('discord-cross-hosting');
-const BOT_TOKEN = process.env.BOT_TOKEN
-const BOT_AUTH_TOKEN = process.env.BOT_AUTH_TOKEN
-const BOT_BRIDGE_PORT = process.env.BOT_BRIDGE_PORT || 4444
-const BOT_TOTAL_SHARDS = process.env.BOT_TOTAL_SHARDS || 4
-const BOT_TOTAL_MACHINES = process.env.BOT_TOTAL_SHARDS || 2
-const BOT_SHARD_PER_CLUSTER = process.env.BOT_SHARD_PER_CLUSTER || 2
-const server = new Bridge({
-    port: +BOT_BRIDGE_PORT, // The Port of the Server | Proxy Connection (Replit) needs Port 443
-    authToken: BOT_AUTH_TOKEN,
-    totalShards: +BOT_TOTAL_SHARDS, // The Total Shards of the Bot or 'auto'
-    totalMachines: +BOT_TOTAL_MACHINES, // The Total Machines, where the Clusters will run
-    shardsPerCluster: +BOT_SHARD_PER_CLUSTER, // The amount of Internal Shards, which are in one Cluster
-    token: BOT_TOKEN,
-});
-
-//server.on('debug', console.log);
-server.start();
-server.on('ready', url => {
-    console.log('Server is ready' + url);
-});
+const socketServer = require('./socket')
+const HEALTH_PORT = process.env.HEALTH_PORT || 3000
+const express = require('express')
+const compression = require('compression')
+const bodyParser = require('body-parser');
+const app = express()
+app.use(bodyParser.json({
+  limit: '500MB',
+  verify: (req, res, buf)=>{
+    req.rawBody = buf.toString()
+  }
+}))
+app.use(compression())
+app.get('/healthz', (req, res)=>{
+  res.status(200).json({status: 'ok'})
+})
+const server = app.listen(HEALTH_PORT, ()=>{
+  console.log('bot bridge is listening on '+server.address()?.port)
+  socketServer(server)
+})
---
+++ src/socket.js
@@ -0,0 +1,116 @@
+const { Server } = require('socket.io')
+const BOT_TOTAL_SHARDS = +process.env.BOT_TOTAL_SHARDS || 2
+const BOT_TOKEN = process.env.BOT_TOKEN
+const BOT_NODE_NAME_PREFIX = process.env.BOT_NODE_NAME_PREFIX || 'bot'
+const SOCKET_EMIT_TIMEOUT = process.env.SOCKET_EMIT_TIMEOUT || 10000
+let enumShardNum = {}
+const remoteSockets = {}
+
+const enumShards = ()=>{
+  try{
+    let i = BOT_TOTAL_SHARDS
+    while(i--){
+      enumShardNum[BOT_NODE_NAME_PREFIX+'-'+i] = +i
+    }
+    if(Object.values(enumShardNum).length === 0) setTimeout(enumShards, 5000)
+    console.log(JSON.stringify(enumShardNum))
+  }catch(e){
+    console.error(e);
+    setTimeout(enumShards, 5000)
+  }
+}
+const getBotShardNum = (obj = {}, socket)=>{
+  try{
+    if(!obj.podName || BOT_TOKEN !== obj.botToken) return
+    let res = { totalShards: BOT_TOTAL_SHARDS, shardNum: enumShardNum[obj.podName] }
+    if(res.shardNum >= 0 && res.totalShards >= 0){
+      remoteSockets[obj.podName] = socket
+      return res
+    }
+  }catch(e){
+    throw(e.message || e)
+  }
+}
+const getShardId = async(obj = {})=>{
+  try{
+    if(!obj.sId) return
+    let id = (Number(BigInt(obj.sId) >> 22n) % (+BOT_TOTAL_SHARDS))
+    if(id >= 0) return BOT_NODE_NAME_PREFIX+'-'+id
+  }catch(e){
+    throw(e.message || e);
+  }
+}
+const getRemoteRequest = (cmd, obj = {})=>{
+  if(!remoteSockets[obj.podName] || !remoteSockets[obj.podName]?.connected) throw('Socket Error: connection not available')
+  return new Promise((resolve, reject)=>{
+    try{
+      remoteSockets[obj.podName].timeout(SOCKET_EMIT_TIMEOUT).emit('request', cmd, obj, (err, res)=>{
+        if(err) reject(`Socket Error: ${err.message || err}`)
+        resolve(res)
+      })
+    }catch(e){
+      console.error(e);
+      reject(e.message || e)
+    }
+  })
+}
+const getRemoteRequestAllBots = async(cmd, obj)=>{
+  try{
+    let resArray = [], i = BOT_TOTAL_SHARDS
+    while(i--){
+      let podName = BOT_NODE_NAME_PREFIX+'-'+i
+      if(!remoteSockets[podName]) continue;
+      obj.podName = podName
+      let res = await getRemoteRequest(podName, cmd, obj)
+      if(res) resArray.push(res)
+    }
+    return resArray
+  }catch(e){
+    console.error(e);
+  }
+}
+const identifySocket = (obj = {}, socket)=>{
+  try{
+    if(obj.podName){
+      remoteSockets[obj.podName] = socket
+      return({status: 'ok'})
+    }
+  }catch(e){
+    throw(e.message || e)
+  }
+}
+const processRequest = async(cmd, obj = {}, socket)=>{
+  try{
+    if(cmd === 'identify') return await identifySocket(obj, socket)
+    if(cmd === 'getBotShardNum') return await getBotShardNum(obj, socket)
+    let podName = obj.podName
+    if(!podName) podName = await getShardId(obj)
+    if(podName === 'all') return await getRemoteRequestAllBots(cmd, obj)
+    if(podName){
+      obj.podName = podName
+      return await getRemoteRequest(cmd, obj)
+    }
+  }catch(e){
+    throw(e.message || e)
+  }
+}
+enumShards()
+module.exports = (server)=>{
+  try{
+    io = new Server(server, {maxHttpBufferSize: 1e8, cors: { origin: ['*']}})
+    console.log('bot bridge socket server is listening on port '+server.address()?.port)
+    io.on('connection', (socket)=>{
+      socket.on('request', async(cmd, obj = {}, callback)=>{
+        try{
+          let res = await processRequest(cmd, obj, socket)
+          if(callback) callback(res)
+        }catch(e){
+          console.log(e.message || e)
+          if(callback) callback()
+        }
+      })
+    })
+  }catch(e){
+    console.log(e)
+  }
+}
